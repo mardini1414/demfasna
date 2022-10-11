cc167a5b851d0239d60948f0ae07181ca8f701b0
Galuh Project <galuhproject1@gmail.com>
2022-10-11 09:03:09 +0700

    feat: navbar, footer

---
+++ resources/js/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Logo from '/DEMFASNA/demfasna/public/assets/logo.png';
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="text-white text-start lg:text-left bg-gradient-to-r from-red-400 to-red-600">
+        <div className="px-24 py-5 text-center md:text-left">
+          <div className="grid gap-10 grid-1 md:grid-cols-2 lg:grid-cols-2 ">
+            <div className="grid grid-cols-1 md:w-12">
+              <img src={Logo} objectFit="contain" width={50} height={50} />
+              <span className="self-center text-xl font-semibold text-black lg:absolute lg:mt-2 md:absolute md:-mt-24 md:ml-14 sm:absolute sm:mt-2">
+                DEMFASNA
+              </span>
+              <p className="px-6 text-black">lorem ipsum ksksksk kakak</p>
+            </div>
+
+            <div className="mx-auto">
+              <h6 className="flex justify-center py-5 mb-4 font-semibold text-black uppercase md:justify-start">
+                CONTACT
+              </h6>
+            </div>
+          </div>
+          <div className="flex justify-center p-6 border-b border-black lg:justify-between"></div>
+          <div>
+            <p>FB</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
