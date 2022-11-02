ea105e6e12ad5c32024b2c1c59ef583856a37041
Galuh Project <galuhproject1@gmail.com>
2022-11-03 00:16:59 +0700

    fix: footer

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import Logo from '../../assets/img/logo.png';
+import logo from '../../assets/img/logo.png';
 
 const Footer = () => {
   return (
-    <footer className="bg-primary">
-      <div className="container mx-auto">
-        <div className="md:flex md:justify-between">
+    <footer className="py-5 bg-primary">
+      <div className="container mx-auto ">
+        <div className="md:flex md:justify-between lg:mx-20">
           <div className="mb-6 md:mb-0">
             <a href="https://flowbite.com/" className="flex items-center">
-              <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="h-8 mr-3"
-                alt="FlowBite Logo"
-              />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Flowbite
+              <img src={logo} width={40} height={40} className="h-auto sm:h-10" alt="Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap text-light">
+                DEMFASNA
               </span>
             </a>
           </div>
@@ -73,13 +69,12 @@ const Footer = () => {
           </div>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <div className="sm:flex sm:items-center sm:justify-between lg:mx-20">
+          <span className="text-sm text-light sm:text-center dark:text-gray-400">
             © 2022{' '}
             <a href="https://flowbite.com/" className="hover:underline">
-              Flowbite™
+              DEMFASNA™
             </a>
-            . All Rights Reserved.
           </span>
           <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
             <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
