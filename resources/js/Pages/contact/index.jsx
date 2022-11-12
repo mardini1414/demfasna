ec3b5bdd0778e668aaa527e001bea4cce85e60f3
galuhprojet1 <galuhproject1@gmail.com>
2022-11-12 15:42:48 +0700

    fix: contact us

--- resources/js/Pages/contact/index.jsx
+++ resources/js/Pages/contact/index.jsx
@@ -1,19 +1,41 @@
 import { Link } from '@inertiajs/inertia-react';
 import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 import Layout from '../../components/layouts/Layout';
+import ImageSuccess from '../../../assets/img/email success.webp'
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm("xvoydjbq");
+  if (state.succeeded) {
+      return (
+        <Layout>
+          <div className='container mx-auto my-5 px-5 lg:px-20 md:px-10 flex justify-center h-screen'>
+            <div className='space-y-5'>
+              <div className='text-primary md:font-bold md:text-3xl text-xl sm:text-2xl font-base bg-blue'>
+                <h1>Terima Kasih Atas Pesannya</h1>
+              </div>
+              <div>
+              <img src={ImageSuccess} alt='mail success' className='flex w-full shadow-md  md:max-w-md rounded-lg justify-center' />
+              </div>
+            </div>
+            
+          </div>
+        </Layout>
+      );
+  }
   return (
     <Layout>
       <div className='container mx-auto px-5 sm:px-10 my-10 lg:px-20 md:px-10'>
       <div className='my-5 text-center text-2xl text-primary font-bold'>HUBUNGI KAMI</div>
         <div className='flex grid lg:grid-cols-3 gap-8'>
           <div className='col-span-2'>
-          <form>
+          <form onSubmit={handleSubmit}>
               <div className="grid grid-cols-2 gap-4">
                 <div className="form-group mb-6">
                   <input
-                    type="text"
+                    type="name"
+                    id="name"
+                    name='name'
                     className="form-control
                     block
                     w-full
@@ -29,14 +51,20 @@ const Contact = () => {
                     ease-in-out
                     m-0
                     focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                          id="exampleInput123"
                           aria-describedby="emailHelp123"
                           placeholder="First name"
                   />
+                  <ValidationError 
+        prefix="Name" 
+        field="name"
+        errors={state.errors}
+      />
                 </div>
                 <div className="form-group mb-6">
                   <input
-                    type="text"
+                    type="email"
+                    id="email"
+                    name='email'
                     className="form-control
                       block
                       w-full
@@ -52,15 +80,21 @@ const Contact = () => {
                       ease-in-out
                       m-0
                       focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                            id="exampleInput124"
                             aria-describedby="emailHelp124"
                             placeholder="Email"
                   />
+                  <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
                 </div>
               </div>
               <div className="form-group mb-6">
                 <input
-                  type="email"
+                  type="subject"
+                  id="subject"
+                  name='subject'
                   className="form-control block
                     w-full
                     px-3
@@ -75,9 +109,15 @@ const Contact = () => {
                     ease-in-out
                     m-0
                     focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                          id="exampleInput125"
+                          
                           placeholder="Subject"
-                        />
+                  />
+                  <ValidationError 
+                        prefix="Subject" 
+                        field="subject"
+                        errors={state.errors}
+                      />
+                
               </div>
               <div className="form-group mb-6">
                 <textarea
@@ -98,15 +138,22 @@ const Contact = () => {
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
                  "
-                  id="exampleFormControlTextarea13"
+                  id="message"
+                  name='message'
                   rows={4}
                   placeholder="Message"
                   defaultValue={""}
                 />
+                <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
               </div>
 
               <button
                 type="submit"
+                disabled={state.submitting}
                 className="
                   w-full
                   px-6
@@ -140,7 +187,13 @@ const Contact = () => {
             <div className='space-y-3'>
               <h1 className='text-primary/80 text-xl text-semibold'>Follow On:</h1>
               <div className='space-x-4'>
+                <Link>
+                <i className="fa-brands fa-twitter border-2 hover:border-0 border-blue-400  text-3xl hover:bg-blue-400 hover:text-white text-blue-400 bg-white  min-w-wull p-2  transform hover:-translate-y-3 rounded-full duration-500"></i>
+                </Link>
+                <Link>
                 <i className="fab fa-instagram border-2 hover:border-0 border-pink-500 bg-gradient-to-b text-3xl hover:from-indigo-600 hover:via-pink-600 hover:to-yellow-500 min-w-wull hover:text-white bg-white text-pink-600 p-2  transform hover:-translate-y-3 rounded-full duration-500"></i>
+                </Link>
+                
                 <Link href='https://www.instagram.com/demfasna/'>
                 <i className="fa-brands fa-youtube text-3xl bg-white transform hover:-translate-y-3  border-2 p-2 rounded-full duration-500 text-red-500 border-red-500 hover:bg-red-500 hover:text-white"></i>
                 </Link>
